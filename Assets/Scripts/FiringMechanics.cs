1a2d433ee80447c3a378053852be8d8e40bb6c84
matt-mert <mertkucukakinci@gmail.com>
2023-09-08 14:54:34 +0300

    Firing mechanism implementation continues

--- Assets/Scripts/FiringMechanics.cs
+++ Assets/Scripts/FiringMechanics.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +8,32 @@ public class FiringMechanics : MonoBehaviour
     [SerializeField]
     private float firingRange;
     [SerializeField]
+    private float firingCooldown;
+    [SerializeField]
+    private GameObject firingProjectile;
+    [SerializeField]
     private List<string> targetTags;
 
     private HashSet<Transform> enemiesDetected;
+    private Transform closestEnemy;
     private SphereCollider sphereCollider;
     private Transform firingPoint;
+    private WaitForEndOfFrame waitForEndOfFrame;
+    private WaitForSeconds waitForSeconds;
 
     private void Awake()
     {
         enemiesDetected = new HashSet<Transform>();
         sphereCollider = GetComponent<SphereCollider>();
         firingPoint = transform.GetChild(0);
+        sphereCollider.radius = firingRange;
+        waitForEndOfFrame = new WaitForEndOfFrame();
+        waitForSeconds = new WaitForSeconds(firingCooldown);
     }
 
     private void Start()
     {
-        sphereCollider.radius = firingRange;
+        StartCoroutine(FiringCoroutine());
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,6 +50,44 @@ public class FiringMechanics : MonoBehaviour
 
     private void Update()
     {
-        
+        if (enemiesDetected.Count == 0)
+        {
+            closestEnemy = null;
+            return;
+        }
+
+        foreach (Transform enemy in enemiesDetected)
+        {
+            // TODO: if isDead, continue
+
+            if (closestEnemy == null)
+            {
+                closestEnemy = enemy;
+                continue;
+            }
+
+            float previousDist = Vector3.Distance(closestEnemy.position, transform.position);
+            float currentDist = Vector3.Distance(enemy.position, transform.position);
+
+            if (currentDist < previousDist)
+            {
+                closestEnemy = enemy;
+            }
+        }
+    }
+
+    private IEnumerator FiringCoroutine()
+    {
+        while (true)
+        {
+            if (closestEnemy == null)
+            {
+                yield return waitForEndOfFrame;
+                continue;
+            }
+
+
+            yield return waitForSeconds;
+        }
     }
 }
---
+++ Assets/Scripts/Interfaces/IProjectile.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IProjectile
+{
+    Vector3 Direction { get; set; }
+}
